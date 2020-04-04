a132f7847ec7c78f673adf6f9779c83056a13b04
Shubhamjwl <shubh.55jwl@gmail.com>
2020-04-04 22:12:49 +0530

    modified files

--- src/main/java/com/shubh/resource/WelcomeController.java
+++ src/main/java/com/shubh/resource/WelcomeController.java
@@ -1,5 +1,7 @@
 package com.shubh.resource;
 
+import org.jboss.logging.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class WelcomeController {
 	
+	private static  org.slf4j.Logger logger=LoggerFactory.getLogger(WelcomeController.class);
 	@GetMapping(value="/greet/{name}")
 	public String greetMsg(@PathVariable("name") String name) {
-		
+		logger.debug("start  method execution");
+		logger.debug("end method execution");
 		return "good evening:"+name;
 	}
 	
--- src/test/java/com/shubh/ApplicationTests.java
+++ src/test/java/com/shubh/ApplicationTests.java
@@ -1,13 +1,21 @@
 package com.shubh;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApplicationTests {
 
+	private static Logger logger=LoggerFactory.getLogger(ApplicationTests.class);
+	
 	@Test
 	void contextLoads() {
+		logger.debug("test case execution");
+		assertEquals(true, true);
 	}
 
 }
